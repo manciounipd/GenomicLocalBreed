63670f88b42b76c6cd7ae91cf71d48708b8518f8
manciounipd <enrico.mancin@unipd.it>
2025-05-26 09:46:00 +0200

    Delete geno_pipeline/s5_convert_in_all.sh

--- geno_pipeline/s5_convert_in_all.sh
+++
@@ -1,6 +0,0 @@
-# converti con il blup e plink
-cd  impute 
-awk 'NR > 1 {print $1,$3}' genotypes_imp.txt >  impute.snp
-awk 'NR > 1{print $2 ,$1  ,0 ,$3}' snp_info.txt  > impute.map
-# converti in plink 
-
